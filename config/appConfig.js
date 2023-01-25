01b5ce5c660d4f1c9f064ecac767f1788fa7f432
Kyaw Thant Zin <kyawthantzamt@gmail.com>
2023-01-25 21:39:42 +0630

    add config files

---
+++ config/appConfig.js
@@ -0,0 +1,8 @@
+
+const appConfig = {
+    "port": process.env.PORT || "8000"
+}
+
+module.exports = {
+    appConfig
+}
---
+++ config/puppeteerConfig.js
@@ -0,0 +1,46 @@
+const {executablePath} = require("puppeteer")
+
+const chromiumBrowser = {
+    "setting": {
+        headless: true,
+        args: ["--no-sandbox", "--disable-setuid-sandbox", "--window-size=1000,1080", "--use-gl=egl", "--disable-dev-shm-usage"],
+        ignoreDefaultArgs: ["--disable-extensions"],
+        executablePath: executablePath(),
+    },
+}
+
+const chromiumPage = {
+    "setting": {
+        "setCacheEnabled": true,
+        "setViewport": {
+            "width": 390,
+            "height": 844
+        },
+        "setDefaultNavigationTimeout": 0,
+        "setRequestInterception": true,
+        "removeResourceTypes": ["image", "font", "other", "media"],
+        "resourceTypes": {
+            "fetch": "fetch",
+            "script": "script",
+            "xhr": "xhr",
+
+        },
+        "waitUntil": {
+            "networkidle0": "networkidle0",
+            "domcontentloaded": "domcontentloaded"
+        },
+        "scrollSetting": {
+            "size": 500,
+            "delay": 250
+        },
+        "contentSelector": "pre",
+    }
+}
+
+
+
+
+module.exports = {
+    chromiumBrowser,
+    chromiumPage
+}
