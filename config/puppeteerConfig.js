cdda53f25270801be8baae598203a1ef8b8ae4b5
Kyaw Thant Zin <kyawthantzamt@gmail.com>
2023-01-27 09:39:44 +0630

    add Vite Vue JS in views folder for Front-end

--- config/puppeteerConfig.js
+++ config/puppeteerConfig.js
@@ -1,4 +1,4 @@
-const {executablePath} = require("puppeteer")
+const { executablePath } = require("puppeteer")
 
 const chromiumBrowser = {
     "setting": {
@@ -10,31 +10,29 @@ const chromiumBrowser = {
 }
 
 const chromiumPage = {
-    "setting": {
-        "setCacheEnabled": true,
-        "setViewport": {
-            "width": 390,
-            "height": 844
-        },
-        "setDefaultNavigationTimeout": 0,
-        "setRequestInterception": true,
-        "removeResourceTypes": ["image", "font", "other", "media"],
-        "resourceTypes": {
-            "fetch": "fetch",
-            "script": "script",
-            "xhr": "xhr",
+    "setCacheEnabled": true,
+    "setViewport": {
+        "width": 390,
+        "height": 844
+    },
+    "setDefaultNavigationTimeout": 0,
+    "setRequestInterception": true,
+    "removeResourceTypes": ["image", "font", "other", "media"],
+    "resourceTypes": {
+        "fetch": "fetch",
+        "script": "script",
+        "xhr": "xhr",
 
-        },
-        "waitUntil": {
-            "networkidle0": "networkidle0",
-            "domcontentloaded": "domcontentloaded"
-        },
-        "scrollSetting": {
-            "size": 500,
-            "delay": 250
-        },
-        "contentSelector": "pre",
-    }
+    },
+    "waitUntil": {
+        "networkidle0": "networkidle0",
+        "domcontentloaded": "domcontentloaded"
+    },
+    "scrollSetting": {
+        "size": 500,
+        "delay": 250
+    },
+    "contentSelector": "pre",
 }
 
 
