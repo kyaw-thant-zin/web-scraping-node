6dba1b649d853735ce575976494c6576c4693b10
Kyaw Thant Zin <kyawthantzamt@gmail.com>
2023-01-27 09:44:06 +0630

    Update vite.config.js

--- views/vite.config.js
+++ views/vite.config.js
@@ -1,7 +1,45 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VitePluginBrowserSync from 'vite-plugin-browser-sync'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  base: './',
+  plugins: [
+    vue({
+      template: {
+        transformAssetUrls: {
+          includeAbsolute: false,
+        },
+      },
+    }),
+    VitePluginBrowserSync()
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, '/src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, '/src/main.js'),
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`,
+      },
+    },
+  },
+  configureServer: app => {
+    app.use(
+      require("sass").middleware({
+        src: __dirname,
+        dest: __dirname,
+        outputStyle: "compressed",
+        prefix: "/",
+      })
+    );
+  },
+});
